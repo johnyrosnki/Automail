3e917917fa2e1a0fad6a9bad1146974318cf1a3a
johnyrosnki <jassko1509@gmail.com>
2025-03-06 19:05:54 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        string imapHost = "imap.gmail.com";  // Zmień na swój serwer IMAP
+        int imapPort = 993;
+        string smtpHost = "smtp.gmail.com";  // Zmień na swój serwer SMTP
+        int smtpPort = 587;
+        string email = "twoj@email.com";       // Twój e-mail
+        string password = "twojehaslo";
+    }
+}
